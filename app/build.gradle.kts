30e8023dea76f53626ebe388a2006eb5f3754b91
Wzindx <2753761311@qq.com>
2026-05-21 00:54:16 +0800

    chore: bump version to 2.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "com.yang.emperor"
         minSdk = 24
         targetSdk = 36
-        versionCode = 23
-        versionName = "2.2"
+        versionCode = 24
+        versionName = "2.3"
     }
 
     buildTypes {
